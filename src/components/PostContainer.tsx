f2de6446b665f04792ac41a500631765aae802b3
rasnikolay <ras.nikolay@mail.ru>
2024-09-24 17:55:44 +0300

    add json-server v.0.5

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,14 +1,17 @@
 import  { useState, useEffect  } from 'react';
 import { postAPI } from '../services/PostService';
 import PostItem from './PostItem';
+import { IPost } from '../models/IPost';
 
 const PostContainer = () => {
-    const [limit, setLimit] = useState(8);
+    const [limit, setLimit] = useState(100);
 
     const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllPostsQuery(limit, {
         //longPolling
         pollingInterval: 1000*60,
     });
+
+    const [createPost, {}] = postAPI.useCreatePostMutation();
 
     // useEffect(() => {
     //     setTimeout(() => {
@@ -16,9 +19,15 @@ const PostContainer = () => {
     //     }, 3000)
     // },)
 
+    const handleCreatePost= async () => {
+        const title = prompt();
+        await createPost({title, body: title} as IPost)
+    }
+
     return (
         <div className='post__list'>
             {/* <button onClick={() => refetch()}>REFEATCH</button> */}
+            <button onClick={handleCreatePost}>Добавить пост</button>
             {isLoading && <h1>Идет загрузка...</h1>}
             {error && <h1>Упс, произошла ошибка</h1>}
             {posts && posts.map(post => 
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,9 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IPost } from "../models/IPost";
 
+const jsonPlaceholder = 'https://jsonplaceholder.typicode.com/';
+const jsonServer = 'http://localhost:5000/';
+
 export const postAPI = createApi({
     reducerPath: 'postAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: jsonServer }),
+    tagTypes: ['Post'],
     endpoints: (build) => ({
         fetchAllPosts: build.query<IPost[], number>({
             query: (limit: number = 5) => ({
@@ -11,7 +15,16 @@ export const postAPI = createApi({
                 params: {
                     _limit: limit
                 }
-            })
+            }),
+            providesTags: result => ['Post'],
         }),
+        createPost: build.mutation<IPost, IPost>({
+            query: (post) => ({
+                url: '/posts',
+                method: 'POST',
+                body: post
+            }),
+            invalidatesTags: ['Post'],
+        })
     }),
 });
